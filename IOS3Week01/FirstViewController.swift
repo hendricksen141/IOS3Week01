ce0d4ff76bb25ec68542b9d7619c5e5ece0d36a3
Hendricksen Sutianto <hendricksensutianto@hendricksens-macbook-pro.local>
2017-09-02 09:27:43 +0700

    Initial Commit

---
+++ IOS3Week01/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IOS3Week01
+//
+//  Created by Hendricksen Sutianto on 9/2/17.
+//  Copyright © 2017 Hendricksen Sutianto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
